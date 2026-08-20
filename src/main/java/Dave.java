011564c2bbd3a47123cf73a5ed16a54136b774bf
Jax0312 <jaxonmoh05@gmail.com>
2026-08-21 02:48:52 +0800

    add greeting

--- src/main/java/Dave.java
+++ src/main/java/Dave.java
@@ -1,5 +1,11 @@
+import jdk.swing.interop.SwingInterOpUtils;
+
 public class Dave {
     public static void main(String[] args) {
+
+        String NAME = "Dave";
+        String SEPARATOR = "____________________________________________________________";
+
         String banner = """
                 ____
                 |  _ \\  __ ___   _____\s
@@ -7,6 +13,11 @@ public class Dave {
                 | |_| | (_| |\\ V /  __/
                 |____/ \\__,_| \\_/ \\___|
                 """;
+        System.out.println(SEPARATOR);
         System.out.println(banner);
+        System.out.printf("Hello! I'm %s.\nAt your service!\n", NAME);
+        System.out.println(SEPARATOR);
+        System.out.println("The wind calls. Farewell!");
+        System.out.println(SEPARATOR);
     }
 }
