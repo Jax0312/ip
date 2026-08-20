1022b1b756e097828b0a32633be6454c8cc92a82
Jax0312 <jaxonmoh05@gmail.com>
2026-08-21 03:05:03 +0800

    feat: track list of tasks

--- src/main/java/Dave.java
+++ src/main/java/Dave.java
@@ -1,25 +1,43 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Dave {
 
     static String NAME = "Dave";
     static String SEPARATOR = "____________________________________________________________";
+    static ArrayList<String> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
 
         sendGreetings();
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
             String userIn = scanner.nextLine();
             if (userIn.equals("bye")) {
                 break;
+            } else if (userIn.equals("list")) {
+                listTask();
+            } else {
+                addTask(userIn);
             }
-            // echo
-            System.out.println(SEPARATOR);
-            System.out.println(userIn);
-            System.out.println(SEPARATOR);
         }
         sendByeMessage();
+    }
+
+    private static void listTask() {
+        System.out.println(SEPARATOR);
+        for (int i = 1; i <= tasks.size(); i++) {
+            System.out.printf("%d. %s\n", i, tasks.get(i - 1));
+        }
+        System.out.println(SEPARATOR);
+    }
+
+    private static void addTask(String task) {
+        tasks.add(task);
+        System.out.println(SEPARATOR);
+        System.out.printf("added: %s\n", task);
+        System.out.println(SEPARATOR);
     }
 
     private static void sendGreetings() {
