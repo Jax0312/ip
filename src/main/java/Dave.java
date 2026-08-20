137d0bc2842664b18ce31eaf55b4ac03090ec3c9
Jax0312 <jaxonmoh05@gmail.com>
2026-08-21 02:42:25 +0800

    rename chatbot

---
+++ src/main/java/Dave.java
@@ -0,0 +1,12 @@
+public class Dave {
+    public static void main(String[] args) {
+        String banner = """
+                ____
+                |  _ \\  __ ___   _____\s
+                | | | |/ _` \\ \\ / / _ \\
+                | |_| | (_| |\\ V /  __/
+                |____/ \\__,_| \\_/ \\___|
+                """;
+        System.out.println(banner);
+    }
+}
